1d7edf39790b4c1fde0a46249489ec2d4410abbe
3P4R4 <mendoza124302@gmail.com>
2023-05-25 13:33:19 -0500

    25.5.23

---
+++ src/app/countries/pages/by-country-page/by-country.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'by-country-page',
+  templateUrl: './by-country.component.html',
+  styleUrls: ['./by-country.component.css']
+})
+export class ByCountryPageComponent {
+
+}

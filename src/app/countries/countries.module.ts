768fe8667a608d167c4d9f0fdc696e5f5bac88fa
3P4R4 <mendoza124302@gmail.com>
2023-06-02 13:33:01 -0500

    2.6.23

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -6,6 +6,7 @@ import { CountryPageComponent } from './pages/country-page/country-page.componen
 import { CountriesRoutingModule } from './countries-routing.module';
 import { ByCountryPageComponent } from './pages/by-country-page/by-country.component';
 import { SharedModule } from '../shared/shared.module';
+import { CountryTableComponent } from './components/country-table/country-table.component';
 
 
 
@@ -15,7 +16,8 @@ import { SharedModule } from '../shared/shared.module';
     ByCapitalPageComponent,
     ByRegionPageComponent,
     ByCountryPageComponent,
-    CountryPageComponent
+    CountryPageComponent,
+    CountryTableComponent
   ],
   imports: [
     CommonModule,
